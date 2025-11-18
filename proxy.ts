01a0ab9c966d2b7f2ca5b2e624a505de233c5b0c
Eavheang Oeng <oengeavheang@gmail.com>
2025-11-18 19:40:21 +0700

    Fixing proxy to middleware

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyToken } from '@/lib/auth';
 
-export async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Public routes that don't require authentication
