0e0e8ed05c0186ddd58efdadcba7656ea6300046
Eavheang Oeng <oengeavheang@gmail.com>
2025-11-20 09:23:15 +0700

    Fixed download pdf and text area color

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[80px] w-full rounded-2xl border border-zinc-200 bg-white px-4 py-3 text-base ring-offset-white placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-[80px] w-full rounded-2xl border border-zinc-200 bg-white px-4 py-3 text-base text-zinc-900 ring-offset-white placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
